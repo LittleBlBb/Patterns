47f0fa7221c41bfe38e0670732c9f1302f801011
LittleBlBb <apdragon2014@gmail.com>
2024-09-13 02:19:28 +0300

    Task 6

--- hello_world.rb
+++ hello_world.rb
@@ -1,4 +1,16 @@
-#Методы для работы с 4-м заданием
+#Метод для выбора используемого метода
+def execute_method(method_number, arr)
+	case method_number
+	when 1
+		puts "Минимальный элемент массива: #{minimal_element_of_array(arr)}"
+	when 2
+		puts "Первый положительный элемент массива: #{first_positive_element(arr)}"
+	else
+		puts "Неверный номер метода!"
+	end
+end
+# #Методы для работы с 4-м заданием
+#Метод для подсчета суммы простых делителей
 def sum_of_simple_divisors(number)
 	list_of_simple_divisors = [] #Создаем массив, куда будем пихаь наши найденные простые делители
 	divisor = 2 #Начинаем с двойки, так как 1 не считается простой
@@ -74,8 +86,7 @@ def product_of_divisors_with_digit_sum_less_than_number(number)
 	end
 end
 
-#Методы для 5-го задания
-
+# #Методы для 5-го задания
 #Метод для поиска минимального элемента в массиве
 def minimal_element_of_array(arr)
 	#Проверяем массив на наличие элементов внутри
@@ -109,85 +120,104 @@ def first_positive_element(arr)
 end
 
 #Main
-#База
-puts("Hello, world!")
+# надо - не надо - не выкупил пока что
+# #База
+# puts("Hello, world!")
 
-# Принимаем имя пользователя. Если не передан аргумент, запрашиваем имя у пользователя.
-if ARGV.empty?
-  puts "Как тебя зовут?"
-  name = gets.chomp
-else
-  name = ARGV[0]
+# # Принимаем имя пользователя. Если не передан аргумент, запрашиваем имя у пользователя.
+# if ARGV.empty?
+#   puts "Как тебя зовут?"
+#   name = gets.chomp
+# else
+#   name = ARGV[0]
+# end
+
+# printf("Здравствуй, #{name}\n") # Форматированное приветствие
+
+# # Очищаем ARGV, чтобы оно не мешало использованию gets
+# ARGV.clear
+
+# # Спросим у пользователя его любимый ЯП
+# puts('Какой твой любимый язык программирования?')
+# favorite_lang = gets.chomp.downcase
+
+# # Условие на любимый ЯП
+# case favorite_lang
+# when 'ruby'
+#   puts("#{name}-подлиза")
+# when 'c++'
+#   puts('Гениалыч?')
+# when 'c#', 'java'
+#   puts('Красава')
+# else
+#   puts('Неважно, скоро будет Ruby')
+# end
+
+# #Просим пользователя ввести команду Ruby
+# puts("Введите команду Ruby:")
+# ruby_command = gets.chomp
+
+# #Разбиваем команду на части для дальнейшей проверки, существует ли она в целом.
+# ruby_command_parts = ruby_command.split(' ')
+
+# #Первая часть команды - метод
+# method_name = ruby_command_parts[0].to_sym
+
+# #Проверяем, существует ли метод в классе Kernel(Включает в себя базовые команды типа puts, print)
+# if (Kernel.respond_to?(method_name))
+# 	puts('Смотри, что ты наделал:')
+# 	eval(ruby_command)
+# else
+# 	puts("Команда '#{ruby_command}' не найдена")
+# end
+
+# #Просим пользователя выполнить ввести команду ОС
+# puts("Введите команду ОС:")
+# os_command = gets.chomp
+
+# if system(os_command)
+# 	puts("Выполнено")
+# else
+# 	puts("Ошибка выполнения")
+# end
+
+# #Просим пользователя ввести число
+# puts("Введите число")
+# number = gets.chomp.to_i
+
+# Надо - не надо, пока не выкупил
+# #Выполняем метод для подсчета суммы простых делителей заданного числа
+# sum_of_simple_divisors(number)
+
+# #Выполняем метод для подсчета количества нечетных цифр, больших 3 для заданного числа
+# count_of_odd_numbers_greater_than_three(number)
+
+# #Выполняем метод для подсчета произведения таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа
+# product_of_divisors_with_digit_sum_less_than_number(number)
+
+# test_array = [-4,-3,-2,-1,0,1,2,3,4,5]
+
+# #Задание 5
+# min = minimal_element_of_array(test_array)
+# puts("min: #{min}")
+
+# first_pos = first_positive_element(test_array)
+# puts("first_pos: #{first_pos}")
+
+if ARGV.length != 2
+	puts("Использование программы: ruby file_name.rb <номер метода> <путь к файлу>")
+	exit
 end
 
-printf("Здравствуй, #{name}\n") # Форматированное приветствие
+method_number = ARGV[0].to_i
+file_path = ARGV[1]
 
-# Очищаем ARGV, чтобы оно не мешало использованию gets
-ARGV.clear
+#Чтение массива из файла
+begin
+	#Считываем содержимое из файла
+	file_content = File.read(file_path)
+	arr = file_content.split.map(&:to_i)
 
-# Спросим у пользователя его любимый ЯП
-puts('Какой твой любимый язык программирования?')
-favorite_lang = gets.chomp.downcase
-
-# Условие на любимый ЯП
-case favorite_lang
-when 'ruby'
-  puts("#{name}-подлиза")
-when 'c++'
-  puts('Гениалыч?')
-when 'c#', 'java'
-  puts('Красава')
-else
-  puts('Неважно, скоро будет Ruby')
+	#Выполняем выбранный метод
+	execute_method(method_number, arr)
 end
-
-#Просим пользователя ввести команду Ruby
-puts("Введите команду Ruby:")
-ruby_command = gets.chomp
-
-#Разбиваем команду на части для дальнейшей проверки, существует ли она в целом.
-ruby_command_parts = ruby_command.split(' ')
-
-#Первая часть команды - метод
-method_name = ruby_command_parts[0].to_sym
-
-#Проверяем, существует ли метод в классе Kernel(Включает в себя базовые команды типа puts, print)
-if (Kernel.respond_to?(method_name))
-	puts('Смотри, что ты наделал:')
-	eval(ruby_command)
-else
-	puts("Команда '#{ruby_command}' не найдена")
-end
-
-#Просим пользователя выполнить ввести команду ОС
-puts("Введите команду ОС:")
-os_command = gets.chomp
-
-if system(os_command)
-	puts("Выполнено")
-else
-	puts("Ошибка выполнения")
-end
-
-#Просим пользователя ввести число
-puts("Введите число")
-number = gets.chomp.to_i
-
-#Выполняем метод для подсчета суммы простых делителей заданного числа
-sum_of_simple_divisors(number)
-
-#Выполняем метод для подсчета количества нечетных цифр, больших 3 для заданного числа
-count_of_odd_numbers_greater_than_three(number)
-
-#Выполняем метод для подсчета произведения таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа
-product_of_divisors_with_digit_sum_less_than_number(number)
-
-test_array = [-4,-3,-2,-1,0,1,2,3,4,5]
-
-#Задание 5
-min = minimal_element_of_array(test_array)
-puts("min: #{min}")
-
-first_pos = first_positive_element(test_array)
-puts("first_pos: #{first_pos}")
-

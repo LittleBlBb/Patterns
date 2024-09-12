b05a453558a46f56b7c7781d2f0e28e9b098e232
LittleBlBb <apdragon2014@gmail.com>
2024-09-13 00:44:46 +0300

    Task 4.3

--- hello_world.rb
+++ hello_world.rb
@@ -17,6 +17,7 @@ def sum_of_simple_divisors(number)
 	#Выводим сумму
 	puts("Сумма простых делителей числа равна #{list_of_simple_divisors.sum}")
 end
+
 #Метод для подсчета нечетных цифр, больших тройки в числе.
 def count_of_odd_numbers_greater_than_three(number)
 	number_of_numbers_found = 0
@@ -28,10 +29,53 @@ def count_of_odd_numbers_greater_than_three(number)
 		end
 		number /= 10 #Избавляемся от последней цифры числа
 	end
-	puts("Количество нечетных цифр числа, больших равно #{number_of_numbers_found}")
+	puts("Количество нечетных цифр числа, больших 3 равно #{number_of_numbers_found}")
 end
 
-# База
+#Метод для нахождения произведения таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа 
+def product_of_divisors_with_digit_sum_less_than_number(number)
+	if(number == 0) 
+		puts("Вы ввели число 0, какой результат вы ожидаете?")
+		return
+	else
+		list_of_found_divisors = []
+		original_number = number
+		original_digit_sum = 0
+
+		#Находим сумму цифр исходного числа
+		temp_number = number
+		while(temp_number != 0)
+			original_digit_sum += temp_number % 10
+			temp_number /= 10
+		end
+
+		#Проходим по всем делителям числа
+		(1..number).each do |i|
+			if (number % i == 0)
+				divisor_digit_sum = 0
+				temp = i
+
+				#Считаем сумму цифр делителя
+				while(temp != 0)
+					divisor_digit_sum += temp % 10
+					temp /= 10
+				end
+
+				#Проверяем условие, добавляем делитель в список
+				if divisor_digit_sum < original_digit_sum
+          list_of_found_divisors.push(i)
+        end
+			end
+		end
+	#Если список пуст, произведение равно 1 (ничего не найдено)
+	product = list_of_found_divisors.empty? ? 1 : product = list_of_found_divisors.inject(1) { |product, n| product * n }
+	
+	puts("Произведение делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа #{product}")
+	end
+end
+
+#Main
+#База
 puts("Hello, world!")
 
 # Принимаем имя пользователя. Если не передан аргумент, запрашиваем имя у пользователя.
@@ -100,3 +144,6 @@ sum_of_simple_divisors(number)
 
 #Выполняем метод для подсчета количества нечетных цифр, больших 3 для заданного числа
 count_of_odd_numbers_greater_than_three(number)
+
+#Выполняем метод для подсчета произведения таких делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа
+product_of_divisors_with_digit_sum_less_than_number(number)

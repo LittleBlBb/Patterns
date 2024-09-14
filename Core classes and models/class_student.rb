c7df4c93d98b5512016a6ea5216e22c70c10b6b8
LittleBlBb <apdragon2014@gmail.com>
2024-09-15 01:52:00 +0300

    Lab2-Task1.1

---
+++ Core classes and models/class_student.rb
@@ -0,0 +1,59 @@
+class Student
+	#Удобная штука, которая автоматически создает геттеры, сеттеры для всех полей
+	attr_accessor :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
+
+	#Конструктор
+	def initialize(id:, first_name:, last_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil)
+		@id = id
+		@last_name = last_name
+		@first_name = first_name
+		@middle_name = middle_name
+		@phone = phone
+		@telegram = telegram
+		@email = email
+		@github = github
+	end
+
+	#Пример написания геттера без использования attr_accessor
+	def id
+		@id
+	end
+	
+	#Пример написания Сеттера без использования attr_accessor
+	def id=(value)
+		@id = value
+	end
+
+	#Метод для вывода информации о студенте
+	def display_info
+		puts("ID: #{@id}")
+		puts("Фамилия: #{@last_name}")
+		puts("Имя: #{@first_name}")
+		puts("Отчество: #{@middle_name}")
+		puts("Телефон: #{@phone}") if @phone
+		puts("Телеграм: #{@telegram}") if @telegram
+		puts("Почта: #{@email}") if @email
+		puts("Github: #{@github}") if @github
+	end
+end
+
+#Создание экземпляра класса Student
+student = Student.new(
+	first_name: "Андрей",
+	last_name: "Пшеничнов",
+	middle_name: "Александрович",
+	id: 1,
+	phone: "+7952812525",
+	telegram: "@test_telegram",
+	email: "apdragon2014@gmail.com",
+	github: "https://github.com/LittleBlBb"
+)
+student.display_info
+
+#Пример использования сеттера и геттера
+student.id= 52
+puts("\nID = #{student.id}")
+student.telegram= "changed_tg"
+puts("telegram: #{student.telegram}")
+
+student.display_info

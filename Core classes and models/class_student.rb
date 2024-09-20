2d4ca13bd30fb0a265ba3c9674041e89f3431039
LittleBlBb <apdragon2014@gmail.com>
2024-09-20 20:43:39 +0300

    Lab2-Task2.5

--- Core classes and models/class_student.rb
+++ Core classes and models/class_student.rb
@@ -1,259 +1,182 @@
 class Student
-	#Удобная штука, которая автоматически создает геттеры, сеттеры для всех полей
-	attr_accessor :id
-	#Автоматом делает геттеры
-	attr_reader :phone, :telegram, :email, :github, :last_name, :first_name, :middle_name
-	#Конструктор
-	def initialize(id: nil, first_name:, last_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil)
-		@id = id
-		@last_name = last_name
-		@first_name = first_name
-		@middle_name = middle_name
-		self.phone = phone if phone #Валидация в сеттере
-		self.telegram = telegram if telegram #Валидация в сеттере
-		self.email = email if email #Валидация в сеттере
-		self.github = github if github #Валидация в сеттере
-	end
+  attr_accessor :id
+  attr_reader :phone, :telegram, :email, :github, :last_name, :first_name, :middle_name
 
-	#Строковое представление объекта
-	def to_s
-		str = []
-		str << "ID: #{@id}" if @id
-		str << "Имя: #{@first_name}"
-		str << "Фамилия: #{@last_name}"
-		str << "Отчество: #{@middle_name}"
-		str << "Телефон: #{@phone}" if phone
-		str << "Телеграм: #{@telegram}" if telegram
-		str << "Почта: #{@email}" if email
-		str << "GitHub: #{@github}" if github
-		str.join(';')
-	end
+  def initialize(id: nil, first_name:, last_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil)
+    @id = id
+    @last_name = last_name
+    @first_name = first_name
+    @middle_name = middle_name
+    self.phone = phone if phone
+    self.telegram = telegram if telegram
+    self.email = email if email
+    self.github = github if github
+  end
 
-	def get_info
-		info = []
-		info << "Инициалы: #{@last_name} #{@first_name[0]}.#{@middle_name[0]}."
-		info << "GitHub: #{@github}" if github
-		if phone
-			info << "Телефон: #{@phone}"
-		elsif email
-			info << "Почта: #{@email}"
-		elsif telegram
-			info << "Телеграм: #{@telegram}"
-		end
-		info.join(';')
-	end
+  def to_s
+    str = []
+    str << "ID: #{@id}" if @id
+    str << "Имя: #{@first_name}"
+    str << "Фамилия: #{@last_name}"
+    str << "Отчество: #{@middle_name}"
+    str << "Телефон: #{@phone}" if phone
+    str << "Телеграм: #{@telegram}" if telegram
+    str << "Почта: #{@email}" if email
+    str << "GitHub: #{@github}" if github
+    str.join('; ')
+  end
 
-	def self.from_string(str)
-		#Создаем пустой хэш
-		data = {}
+  def get_info
+    info = []
+    info << "Инициалы: #{@last_name} #{@first_name[0]}.#{@middle_name[0]}."
+    info << "GitHub: #{@github}" if github
+    info << contact_info
+    info.compact.join('; ')
+  end
 
-		#Парсим строку по "; " и извлекаем ключи и значения
-		str.split('; ').each do |pair|
-			key, value = pair.split(': ').map(&:strip)
-			#Заполняем хэш
-			case key
-			when "ID"
-				data[:id] = value.to_i
-			when "Имя"
-				data[:first_name] = value
-			when "Фамилия"
-				data[:last_name] = value
-			when "Отчество"
-				data[:middle_name] = value
-			when "Телефон"
-				data[:phone] = value
-			when "Телеграм"
-				data[:telegram] = value
-			when "Почта"
-				data[:email] = value
-			when "GitHub"
-				data[:github] = value
-			end
-		end
+  def contact_info
+    if phone
+      "Телефон: #{@phone}"
+    elsif email
+      "Почта: #{@email}"
+    elsif telegram
+      "Телеграм: #{@telegram}"
+    end
+  end
 
-		#Передаем хэш с параметрами в конструктор(** отвечает за развертывание хэша, превращая его ключи в аргументы конструктора)
-		self.new(**data)
-	end
+  def self.from_string(str)
+    data = {}
+    str.split('; ').each do |pair|
+      key, value = pair.split(': ').map(&:strip)
+      case key
+      when "ID"
+        data[:id] = value.to_i
+      when "Имя"
+        data[:first_name] = value
+      when "Фамилия"
+        data[:last_name] = value
+      when "Отчество"
+        data[:middle_name] = value
+      when "Телефон"
+        data[:phone] = value
+      when "Телеграм"
+        data[:telegram] = value
+      when "Почта"
+        data[:email] = value
+      when "GitHub"
+        data[:github] = value
+      when "Инициалы"
+        initials = value.split(' ')
+        data[:last_name] = initials[0]
+        initials_split = initials[1].split('.')
+        data[:first_name] = initials_split[0]
+        data[:middle_name] = initials_split[1]
+      end
+    end
+    data
+  end
 
-	#Метод для вывода информации о студенте
-	def display_info
-		puts("ID: #{@id}") if @id
-		puts("Фамилия: #{@last_name}")
-		puts("Имя: #{@first_name}")
-		puts("Отчество: #{@middle_name}")
-		puts("Телефон: #{@phone}") if @phone
-		puts("Телеграм: #{@telegram}") if @telegram
-		puts("Почта: #{@email}") if @email
-		puts("GitHub: #{@github}") if @github
-	end
+  def display_info
+    puts("ID: #{@id}") if @id
+    puts("Фамилия: #{@last_name}")
+    puts("Имя: #{@first_name}")
+    puts("Отчество: #{@middle_name}")
+    puts("Телефон: #{@phone}") if @phone
+    puts("Телеграм: #{@telegram}") if @telegram
+    puts("Почта: #{@email}") if @email
+    puts("GitHub: #{@github}") if @github
+  end
 
-	#Валидация номера телефона
-	def self.phone_valid?(phone)
-		#Проверяем, что phone - это строка
-		return false unless phone.is_a?(String)
+  private
 
-		#Проверяем номер длиной 11 символов
-		if phone.length == 11
-			return phone.match?(/^\d+$/) #Проверка, что все символы - цифры, если номер начинается с 8
-		#Проверяем номер длиной 12 символов с кодом страны
-		elsif phone.length == 12
-			return phone.match?(/^\+\d+$/) #Первый +, останое - цифры
-		else
-			return false
-		end	
-	end
+  def phone=(phone)
+    if Student.phone_valid?(phone)
+      @phone = phone
+    else
+      puts "Invalid phone"
+    end
+  end
 
-	#Сеттер гита с валидацией
-	def github=(github)
-		if Student.github_valid?(github)
-			@github = github
-		else
-			puts "Invalid GitHub"
-		end
-	end
+  def telegram=(telegram)
+    if Student.telegram_valid?(telegram)
+      @telegram = telegram
+    else
+      puts "Invalid telegram"
+    end
+  end
 
-	#Валидация почты
-	def self.email_valid?(email)
-		email.match?(/\A[^@\s]+@[^@\s]+\.[^@\s]+\z/)
-	end
+  def email=(email)
+    if Student.email_valid?(email)
+      @email = email
+    else
+      puts "Invalid email"
+    end
+  end
 
-	#Валидация телеги
-	def self.telegram_valid?(telegram)
-		telegram.match?(/\A@[a-zA-Z0-9_]{5,}\z/)
-	end
+  def github=(github)
+    if Student.github_valid?(github)
+      @github = github
+    else
+      puts "Invalid GitHub"
+    end
+  end
 
-	#Валидация гитхаба
-	def self.github_valid?(github)
-		github.match?(/\Ahttps:\/\/github.com\/[a-zA-Z0-9_-]+\z/)
-	end
+  # Валидации
+  def self.phone_valid?(phone)
+    phone.match?(/^\+?\d{10,12}$/)
+  end
 
-	#Проверяем, присутствует ли хотя бы один контакт студента
-	def contact_present?
-		@phone || @phone || @email
-	end
+  def self.email_valid?(email)
+    email.match?(/\A[^@\s]+@[^@\s]+\.[^@\s]+\z/)
+  end
 
-	#Проверяем, присутствует ли гит у студента
-	def git_present?
-		@github
-	end
+  def self.telegram_valid?(telegram)
+    telegram.match?(/\A@[a-zA-Z0-9_]{5,}\z/)
+  end
 
-	#Метод, проводящий две валидации: наличие гита и наличие любого контакта для связи
-	def validate
-		errors = []
-		errors << "GitHub не указан в профиле студента" unless git_present?
-		errors << "Контакты не указаны в профиле студента" unless contact_present?
-		
-		if errors.empty?
-			puts "Валидация пройдена"
-		else
-			puts "Валидация не пройдена\nОшибки валидации:"
-			errors.each{|error| puts error}
-		end
-	end
-
-	#Метод для установки значений контактов
-	def set_contacts(phone: nil, email: nil, telegram: nil)
-		self.phone = phone
-		self.email = email
-		self.telegram = telegram
-	end
-	
-	private
-	
-	#Сеттер номера с валидацией
-	def phone=(phone)
-		if Student.phone_valid?(phone)
-			@phone = phone
-		else
-			puts "Invalid phone"
-		end
-	end
-
-	#Сеттер телеги с валидацией
-	def telegram=(telegram)
-		if Student.telegram_valid?(telegram)
-			@telegram = telegram
-		else
-			puts "Invalid telegram"
-		end
-	end
-
-	#Сеттер почты с валидацией
-	def email=(email)
-		if Student.email_valid?(email)
-			@email = email
-		else
-			puts "Invalid email"
-		end
-	end
+  def self.github_valid?(github)
+    github.match?(/\Ahttps:\/\/github.com\/[a-zA-Z0-9_-]+\z/)
+  end
 end
 
-class StudentShort
-	attr_reader :id, :initials, :github, :contact
-	def initialize(student)
-		if student.is_a?(Student)
-			@id = student.id if student.id
-			@initials = "#{student.last_name} #{student.first_name[0].upcase}.#{student.middle_name[0].upcase}."
-			@github = student.github if student.github
-			@contact = 
-			if student.phone
-				"Телефон: #{student.phone}"
-			elsif student.email
-				"Почта: #{student.email}"
-			elsif student.telegram
-				"Телеграм: #{student.telegram}"
-			end
-		elsif student.is_a?(String)
-			data = self.class.from_string(student)
-			@id = data[:id] if data[:id]
-			@initials = data[:initials]
-			@github = data[:github] if data[:github]
-			@contact = data[:contact] if data[:contact]
-		end
-	end
+class StudentShort < Student
+  attr_reader :initials, :contact
 
-	def self.from_string(str)
-		data = {}
+  def initialize(student)
+    if student.is_a?(Student)
+      super(
+        id: student.id,
+        first_name: student.first_name,
+        last_name: student.last_name,
+        middle_name: student.middle_name,
+        phone: student.phone,
+        telegram: student.telegram,
+        email: student.email,
+        github: student.github
+      )
+      @initials = "#{student.last_name} #{student.first_name[0].upcase}.#{student.middle_name[0].upcase}."
+      @contact = student.contact_info
+    elsif student.is_a?(String)
+      data = Student.from_string(student)
+      super(
+        id: data[:id],
+        first_name: data[:first_name],
+        last_name: data[:last_name],
+        middle_name: data[:middle_name],
+        phone: data[:phone],
+        telegram: data[:telegram],
+        email: data[:email],
+        github: data[:github]
+      )
+      @initials = "#{@last_name} #{@first_name[0].upcase}.#{@middle_name[0].upcase}."
+      @contact = contact_info
+    end
+  end
 
-		#Разбираем строку, заполняем хэш
-		str.split(';').each do |pair|
-			key, value = pair.split(': ').map(&:strip)
-			case key
-			when "Инициалы"
-				data[:initials] = value
-			when "GitHub"
-				data[:github] = value
-			when "Телефон", "Почта", "Телеграм"
-				data[:contact] = "#{key}: #{value}"
-			when "ID"
-				data[:id] = value.to_i
-			end
-		end
-		data
-	end
-
-	def display_info
-		puts "ID: #{@id}" if id
-		puts "Инициалы: #{@initials}"
-		puts "GitHub: #{@github}" if github
-		puts "#{@contact}" if contact
-	end
+  def display_info
+    puts "ID: #{@id}" if id
+    puts "Инициалы: #{@initials}"
+    puts "GitHub: #{@github}" if @github
+    puts "#{@contact}" if @contact
+  end
 end
-
-#Создание через объект Student
-student = Student.new(
-	first_name: "Андрей",
-	last_name: "Пшеничнов",
-	middle_name: "Александрович",
-	id: 1,
-	phone: "+79528125252",
-	telegram: "@test_telegram",
-	email: "apdragon2014@gmail.com",
-	github: "https://github.com/LittleBlBb"
-	)
-student2 = StudentShort.new(student)
-student2.display_info
-
-#Создание через строку
-student3 = StudentShort.new(student.get_info)
-student3.display_info
--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -1,69 +1,24 @@
 #Подключаем файл с классом Student
 require_relative 'class_student'
 
-#Создание нескольких объектов Student
-student1 = Student.new(
-	first_name: "Андрей",
-	last_name: "Пшеничнов",
-	middle_name: "Александрович",
-	id: 1,
-	phone: "+79528125252",
-	telegram: "@test_telegram",
-	email: "apdragon2014@gmail.com",
-	github: "https://github.com/LittleBlBb"
-	)
-student2 = Student.new(
-	first_name: "Виталий",
-	last_name: "Дука",
-	middle_name: "Андреевич",
-	id: 2,
-	phone: "+71234******",
-	telegram: "@test_telegram2",
-	email: "duka@mail.ru",
-	github: "https://github.com/sdnezz"
-	)
-student3 = Student.new(
-	first_name: "Андрей",
-	last_name: "Дерябин",
-	middle_name: "Александрович",
-	id: 3
-	)
-#Вывод информации о студентах
-student1.display_info
+
+# Тестирование
+
+# Создание через объект Student
+student = Student.new(
+  first_name: "Андрей",
+  last_name: "Пшеничнов",
+  middle_name: "Александрович",
+  id: 1,
+  phone: "+79528125252",
+  telegram: "@test_telegram",
+  email: "apdragon2014@gmail.com",
+  github: "https://github.com/LittleBlBb"
+)
+
+student2 = StudentShort.new(student)
 student2.display_info
+
+# Создание через строку
+student3 = StudentShort.new(student.get_info)
 student3.display_info
-
-#Пример использования сеттера и геттера
-student1.id = 52
-puts("Student's ID: #{student1.id}")
-student1.display_info
-
-#Попытка задать некорректные значения
-# student1.phone = "12345"       -> Ошибка
-# student1.email = "wrong.email" -> Ошибка
-# student1.telegram = "tg"       -> Ошибка
-student1.github = "github.com/LittleBlBb"
-
-#Тест пройденой валидации
-student1.validate
-
-#Тест проваленой валидации
-student3.validate
-
-#Тест метода set_contacts
-student2.set_contacts(
-	email: "testemail@example.ru",
-	telegram: "@testtelegram",
-	phone: "+79912345436"
-	)
-student2.display_info
-
-# #Тест метода преобразования в строку и конструктора, который парсит данные из строки
-#Пример строки
-student_string = "ID: 1; Имя: Андрей; Фамилия: Пшеничнов; Отчество: Александрович; Телефон: +79528125252; Телеграм: @test_telegram; Почта: apdragon2014@gmail.com; GitHub: https://github.com/LittleBlBb"
-
-#Создание объекта через строку
-student4 = Student.from_string(student_string)
-
-#Выводим информацию о студенте
-student4.display_info

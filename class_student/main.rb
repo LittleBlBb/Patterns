44dabecc7d586484f00ecdc7d13847ce82f66bf9
LittleBlBb <apdragon2014@gmail.com>
2024-10-13 18:42:09 +0300

    Lab2 - Refactoring -Добавил класс "StudentBase" -Теперь класс StudentShort меньше, чем Student -Перенес методы чтения/записи в файл в main.rb -Убрал дублирование кода

--- class_student/main.rb
+++ class_student/main.rb
@@ -1,5 +1,32 @@
-# Подключаем файл с классом Student
+# Подключаем файл с классами
 require_relative 'student'
+
+#Перенес методы, мол че их пихать в классы, если они есть в рельсах
+def read_from_txt(path)
+  begin
+    raise "Файл не найден или некорректный путь: #{path}" unless File.exist?(path)
+    students = []
+    File.readlines(path).each do |line|
+      next if line.strip.empty?
+      students << Student.from_string(line.strip)
+    end
+    students
+  rescue => e
+    puts "Ошибка: #{e.message}"
+  end
+end
+
+def write_to_txt(path, students)
+  begin
+    File.open(path, 'w') do |file|
+      students.each do |student|
+        file.puts student.to_s
+      end
+    end
+  rescue => e
+    puts "Ошибка записи в файл: #{e.message}"
+  end
+end
 
 # Тестирование
 
@@ -27,14 +54,14 @@ puts "Вывод студента 3: \n#{student3}"
 # Тест для read_from_txt
 puts "Вывод студентов из файла:\n"
 path = "from_txt.txt"  # Указываем путь к файлу
-students_from_txt = Student.read_from_txt(path)
+students_from_txt = read_from_txt(path)
 students_from_txt.each do |stud|
   puts stud
 end
 
 # Тест для write_to_txt
-write_path = "C:/Users/Kertis/Desktop/Ruby/class_student/to_txt.txt"  # Путь для записи в файл
-Student.write_to_txt(write_path, students_from_txt)
+write_path = "to_txt.txt"  # Путь для записи в файл
+write_to_txt(write_path, students_from_txt)
 
 # Тест для ошибок в методе
 begin
--- class_student/student.rb
+++ class_student/student.rb
@@ -1,14 +1,46 @@
-class Student
-  attr_accessor :id, :last_name, :middle_name, :first_name
-  attr_reader :phone, :telegram, :email, :github
+class StudentBase
+  attr_accessor :id
+  attr_reader :github
 
-  def initialize(first_name:, last_name:, middle_name:, phone: nil, telegram: nil, email: nil, github: nil, id: nil)
-    @id = id
-    @last_name = last_name
-    @first_name = first_name
-    @middle_name = middle_name
-    set_contacts(phone: phone, telegram: telegram, email: email) if (phone || telegram || email)
+  def initialize(id: nil, github: nil)
+    self.id = id if id
     self.github = github if github
+  end
+
+  def github=(github)
+    raise ArgumentError unless StudentBase.github_valid?(github)
+    @github = github
+  end
+
+  # Валидация GitHub
+  def self.github_valid?(github)
+    github.match?(/\Ahttps:\/\/github.com\/[a-zA-Z0-9_-]+\z/)
+  end
+end
+
+class Student < StudentBase
+  attr_accessor :first_name, :last_name, :middle_name
+  attr_reader :phone, :email, :telegram
+
+  def initialize(first_name:, last_name:, middle_name:, phone: nil, email: nil, telegram: nil, id: nil, github: nil)
+    super(id: id, github: github)
+    self.first_name = first_name
+    self.last_name = last_name
+    self.middle_name = middle_name
+    set_contacts(phone: phone, email: email, telegram: telegram) if phone || email || telegram
+  end
+
+  def set_contacts(phone: nil, email: nil, telegram: nil)
+    self.phone = phone if phone
+    self.email = email if email
+    self.telegram = telegram if telegram
+  end
+
+  def contact_info
+    return "Телефон: #{@phone}" if phone
+    return "Почта: #{@email}" if email
+    return "Телеграм: #{@telegram}" if telegram
+    "Контакт не указан"
   end
 
   def to_s
@@ -32,140 +64,24 @@ class Student
     info.compact.join('; ')
   end
 
-  def contact_info
-    if phone
-      "Телефон: #{@phone}"
-    elsif email
-      "Почта: #{@email}"
-    elsif telegram
-      "Телеграм: #{@telegram}"
-    end
-  end
-
-  def self.from_string(str)
-    data = {}
-    raise "Ошибка парсинга строки: Строка пустая, нечего парсить." if str.nil? || str.strip.empty?
-    str.split('; ').each do |pair|
-      key, value = pair.split(': ').map(&:strip)
-      case key
-      when "ID"
-        data[:id] = value.to_i
-      when "Имя"
-        data[:first_name] = value
-      when "Фамилия"
-        data[:last_name] = value
-      when "Отчество"
-        data[:middle_name] = value
-      when "Телефон"
-        data[:phone] = value
-      when "Телеграм"
-        data[:telegram] = value
-      when "Почта"
-        data[:email] = value
-      when "GitHub"
-        data[:github] = value
-      when "Инициалы"
-        initials = value.split(' ')
-        data[:last_name] = initials[0]
-        initials_split = initials[1].split('.')
-        data[:first_name] = initials_split[0]
-        data[:middle_name] = initials_split[1]
-      end
-    end
-    Student.new(**data)
-  end
-
-  def set_contacts(phone: nil, email: nil, telegram: nil)
-    self.phone = phone if phone
-    self.email = email if email
-    self.telegram = telegram if telegram
-  end
-
-  def self.read_from_txt(path)
-    begin
-      #Выкидываем ошибку
-      raise "Файл не найден или некоррекный путь: #{path}" unless File.exist?(path)
-      students = []
-      #Читаем файл построчно
-      File.readlines(path).each do |line|
-        #Проверяем строки, пустые или нет
-        next if line.strip.empty?
-        students << Student.from_string(line.strip)
-      end
-      students
-    #Записываем объект ошибки в переменную
-    rescue => e
-      puts "Ошибка: #{e.message}"
-    end
-  end
-
-  def self.write_to_txt(path, students)
-    begin
-      File.open(path, 'w') do |file|
-        students.each do |student|
-          file.puts student.to_s
-        end
-      end
-    rescue => e
-      puts "Ошибка записи в файл: #{e.message}"
-    end
-  end
-
   private
 
   def phone=(phone)
-    begin
-      raise "Invalid phone" unless Student.phone_valid?(phone)
-      @phone = phone
-    rescue => e
-      puts "Ошибка валидации телефона: #{e.message}"
-    end
-  end
-
-  def telegram=(telegram)
-    begin
-      raise "Invalid telegram" unless Student.telegram_valid?(telegram)
-      @telegram = telegram
-    rescue => e
-      puts "Ошибка валидации телеграма: #{e.message}"
-    end
+    raise ArgumentError unless Student.phone_valid?(phone)
+    @phone = phone
   end
 
   def email=(email)
-    begin 
-      raise "Invalid email" unless Student.email_valid?(email)
-      @email = email
-    rescue => e
-      puts "Ошибка валидации почты: #{e.message}"
-    end
+    raise ArgumentError unless Student.email_valid?(email)
+    @email = email
   end
 
-  def github=(github)
-    begin
-      raise "Invalid GitHub" unless Student.github_valid?(github)
-      @github = github
-    rescue => e
-      puts "Ошибка валидации гита: #{e.message}"
-    end
+  def telegram=(telegram)
+    raise ArgumentError unless Student.telegram_valid?(telegram)
+    @telegram = telegram
   end
 
   # Валидации
-  def self.id_valid?(id)
-    id.is_a?(Ineger) && id > 0
-  end
-
-  def self.first_name_valid?(first_name)
-    first_name.match?(/\A[А-Яа-яA-Za-z]+(?:-[А-Яа-яA-Za-z]+)?\z/)
-  end
-
-  def self.last_name_valid?(last_name)
-    last_name.match?(/\A[А-Яа-яA-Za-z]+(?:-[А-Яа-яA-Za-z]+)?\z/)
-  end
-
-  def self.middle_name_valid?(middle_name)
-    middle_name.match?(/\A[А-Яа-яA-Za-z]+(?:-[А-Яа-яA-Za-z]+)?\z/)
-  end
-
   def self.phone_valid?(phone)
     phone.match?(/^\+?\d{10,12}$/)
   end
@@ -178,71 +94,88 @@ class Student
     telegram.match?(/\A@[a-zA-Z0-9_]{5,}\z/)
   end
 
-  def self.github_valid?(github)
-    github.match?(/\Ahttps:\/\/github.com\/[a-zA-Z0-9_-]+\z/)
-  end
-end
-
-class StudentShort < Student
-  attr_reader :initials, :contact, :id, :github
-
-  def initialize(student)
-    @id = student.id if student.id
-    @initials = "#{student.last_name} #{student.first_name[0]}.#{student.middle_name[0]}."
-    @contact = student.contact_info || "Контакт не указан"
-    @github = student.github if student.github
-  end
-
-  # Метод from_string для обработки строк разных типов
-  def self.from_string(string, id: nil)
-    data = {}
-
-    # Разбиваем строку на компоненты
-    string.split('; ').each do |pair|
+  def self.from_string(str)
+    data = from_string_base(str)
+    str.split('; ').each do |pair|
       key, value = pair.split(': ').map(&:strip)
       case key
       when "ID"
         data[:id] = value.to_i
-      when "Инициалы"
-        initials = value.split(' ')
-        data[:last_name] = initials[0]
-        data[:first_name], data[:middle_name] = initials[1].split('.').map(&:strip)
+      when "GitHub"
+        data[:github] = value
+      end
+    end
+    Student.new(**data)
+  end
+
+  def self.from_string_base(str)
+    data = {}
+    str.split('; ').each do |pair|
+      key, value = pair.split(': ').map(&:strip)
+      case key
       when "Имя"
         data[:first_name] = value
       when "Фамилия"
         data[:last_name] = value
       when "Отчество"
         data[:middle_name] = value
-      when "Телефон", "Почта", "Телеграм"
-        data[:contact] = value
-      when "GitHub"
-        data[:github] = value
+      when "Телефон"
+        data[:phone] = value
+      when "Почта"
+        data[:email] = value
+      when "Телеграм"
+        data[:telegram] = value
       end
     end
+    data
+  end
+end
 
-    # Создаем объект Student
-    student = Student.new(
-      first_name: data[:first_name],
-      last_name: data[:last_name],
-      middle_name: data[:middle_name],
-      phone: data[:contact],
-      github: data[:github]
-    )
+class StudentShort < StudentBase
+  attr_reader :initials, :contact
 
-    # Устанавливаем ID, если он передан напрямую или найден в строке
-    student.id = data[:id] || id  # Теперь ID устанавливается, даже если оно передано как аргумент
-
-    # Возвращаем объект StudentShort
-    StudentShort.new(student)
+  def initialize(student)
+    super(id: student.id, github: student.github)
+    @initials = "#{student.last_name} #{student.first_name[0]}.#{student.middle_name[0]}."
+    @contact = student.contact_info
   end
 
   def to_s
     str = []
-    str << "ID: #{@id}" if @id  # Вывод ID если он установлен
+    str << "ID: #{@id}" if @id
     str << "Инициалы: #{@initials}"
     str << "Контакт: #{@contact}"
     str << "GitHub: #{@github}" if github
     str.join('; ')
   end
-end
 
+  def self.from_string(str, id: nil)
+    data = Student.from_string_base(str)
+    str.split('; ').each do |pair|
+      key, value = pair.split(': ').map(&:strip)
+      case key
+      when "Инициалы"
+        initials = value.split(' ')
+        data[:last_name] = initials[0]
+        name_parts = initials[1].split('.')
+        data[:first_name] = name_parts[0]
+        data[:middle_name] = name_parts[1]
+      when "GitHub"
+        data[:github] = value
+      end
+    end
+    data[:id] = id if id
+
+    student = Student.new(
+      first_name: data[:first_name],
+      last_name: data[:last_name],
+      middle_name: data[:middle_name],
+      id: data[:id],
+      phone: data[:phone],
+      email: data[:email],
+      telegram: data[:telegram],
+      github: data[:github]
+    )
+    StudentShort.new(student)
+  end
+end

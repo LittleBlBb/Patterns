dbaf40ba38f89c4c2b20feb931858ccea77d9b0d
LittleBlBb <apdragon2014@gmail.com>
2024-10-30 20:58:07 +0300

    Lab3-Task1.25 Method2

--- block_trees/programs/block.rb
+++ block_trees/programs/block.rb
@@ -26,8 +26,19 @@ def move_elements(array, &block)
   block.call(new_array) if block_given?
 end
 
+def max_element_in_interval(array, start_index, end_index, &block)
+  new_array = array[start_index..end_index]
+  max = new_array[0]
+  block.call(new_array) if block_given?
+  new_array.each {|number| max = number if number > max}  
+
+  max
+end
+
 def choose_task
-  puts "Введите номер задачи: 1 - Разместить элементы до минимального в конец массива."
+  puts "Введите номер задачи:\n" + 
+  "1 - Разместить элементы до минимального в конец массива.\n" +
+  "2 - Найти максимальное число в массиве в заданном диапазоне."
     task = gets.chomp.to_i
 
     case task
@@ -36,6 +47,14 @@ def choose_task
       move_elements(array) do |result|
         puts "Result: #{result.inspect}"
       end
+    when 2
+      array = read_input
+      start_index = gets.chomp.to_i
+      end_index = gets.chomp.to_i
+      max = max_element_in_interval(array, start_index, end_index) do |example|
+        puts "Result: #{example}"
+      end
+      puts max
     else
       puts "Неверный выбор. Попробуйте снова"
       choose_task
